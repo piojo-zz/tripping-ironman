7807bfe593bd521f4cfc082a21877c78b58ccc09
Luis Fernando Muñoz Mejías <Luis.Munoz@UGent.be>
2012-12-05 13:05:55 +0100

    Inject several messages on the same host, PID and timestamp.

---
+++ injected-ssh.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+#!/usr/bin/python
+'''Script generating lots of messages for some stupid programs'''
+import os
+import sys
+from attack import basic_options, anomaly, syslog_printer, message_burst
+from random import randint
+
+TROUBLESOME_MESSAGES = [ 'sshd[%d]: error: Bind to port 22 on 0.0.0.0 failed: Address already in use.',
+                        'sshd[%d]: error: Bind to port 22 on :: failed: Address already in use.',
+                        'sshd[%d]: fatal: Cannot bind any address.' ]
+
+def parse_opts():
+    '''Parses the command line'''
+    parser = basic_options()
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    (options, args) = parse_opts()
+
+
+    @anomaly(options.begin, options.end, options.rate, options.nodes)
+    @message_burst(len(TROUBLESOME_MESSAGES))
+    @syslog_printer
+    def generate_ssh_errors(iteration):
+        return TROUBLESOME_MESSAGES[iteration]
+
+
+    generate_ssh_errors()
